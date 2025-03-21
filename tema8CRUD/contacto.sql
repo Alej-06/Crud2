a2eebf0c5a902d53deda42c2cb285c011a7bf537
Alejandro <alejandropm06@gmail.com>
2025-03-21 10:23:15 +0100

    Signed-off-by: Alejandro <alejandropm06@gmail.com>

--- tema8CRUD/contacto.sql
+++ tema8CRUD/contacto.sql
@@ -33,6 +33,16 @@ CREATE TABLE `contacto` (
   UNIQUE KEY `dni_UNIQUE` (`dni`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacto`
+--
+
+LOCK TABLES `contacto` WRITE;
+/*!40000 ALTER TABLE `contacto` DISABLE KEYS */;
+INSERT INTO `contacto` VALUES (1,'ana','martinez',12378946),(2,'ruben','doblas',45678913),(3,'pepe','diaz',23145679),(4,'carla','perez',78945621);
+/*!40000 ALTER TABLE `contacto` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -43,4 +53,4 @@ CREATE TABLE `contacto` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-21 10:14:21
+-- Dump completed on 2025-03-21 10:22:16
